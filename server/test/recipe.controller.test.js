8b079da479de3746a2c8a9d1d0b2f519d05d19f6
Chris Mitchell <christoph.mitchell@gmail.com>
2023-03-11 15:13:19 +0000

    test(recipe controller): switching from jest to vitest
    seems vitest may be more suitable for testing than jest

--- server/test/recipe.controller.test.js
+++ server/test/recipe.controller.test.js
@@ -6,6 +6,7 @@ const recipeModel = require('../models/recipe.model')
 const { PrismaClient } = require('@prisma/client');
 const prisma = require('../models/db-connect');
 const dataBaseName = 'test';
+const { testrecipe, newTotalTime, shortURL } = require('./mocks')
 
 describe('Database connection test', () => {
   const app = express();
@@ -13,23 +14,17 @@ describe('Database connection test', () => {
   app.use(router);
   const request = supertest(app);
 
-  const testrecipe = {
-    title: 'This is a test recipe',
-    prepTime: '1 minute',
-    cookTime: '2 minutes',
-    totalTime: '3.5 minutes',
-    servings: '4 people',
-    instructions:  [{"title":"fakeinstructiontitle","instructions":["fakeinstruction"]}],
-    ingredients:  ["Fake ingredient"]
-  }
+
 
   beforeAll(async () => {
     process.env.DATABASE_URL = 'postgresql://postgres:IrhJ1BrgRiCVrKBV@127.0.0.1:5432/recipedbtest'
+    console.log(process.env.DATABASE_URL)
     const prisma = new PrismaClient();
   })
 
   afterEach(async () => {
     await prisma.recipe.deleteMany({})
+    console.log(process.env.DATABASE_URL)
   })
 
   it('should save a recipe to the database', async () => {
@@ -42,6 +37,35 @@ describe('Database connection test', () => {
 
   })
 
-  
+  it('should update a recipe in the database', async () => {
+
+    const res = await request.post('/recipe/add')
+      .send(testrecipe)
+
+    const recipe = await recipeModel.findRecipe({ title: testrecipe.title })
+
+    testrecipe.id = recipe.id
+    testrecipe.totalTime = newTotalTime
+
+    const res2 = await request.post('/recipe/add')
+    .send(testrecipe)
+
+    const recipe2 = await recipeModel.findRecipe({ title: testrecipe.title })
+    expect(recipe2.totalTime).toBe(newTotalTime)
+  })
+
+  // it('should find a specific recipe', async () => {
+
+  //   testrecipe.url = shortURL
+
+  //    const res = await request.post('/recipe/add')
+  //      .send(testrecipe)
+
+  //   const recipe = await request.post('/recipe/get')
+  //     .send(testrecipe)
+  //  // console.log(recipe)
+
+  //   //expect(recipe.title).toBe(testrecipe.title)
+  // })
 
 })
