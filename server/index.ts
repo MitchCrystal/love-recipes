2ad5c9987c84775f49641585794d1a2ff0df4a6f
Chris Mitchell <christoph.mitchell@gmail.com>
2023-03-15 09:12:47 +0000

    fix(default image): custom recipes were missing an image
    added in a default image for custom recipes

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import {router} from './router';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
@@ -10,6 +11,7 @@ dotenv.config({ path: './.env' });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('staticFiles'))
 app.use(router);
 
 const port = process.env.PORT;
